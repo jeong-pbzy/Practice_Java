6ce71fab850aed89d62c988a75e795a5c3d37fb1
Hannah <jeong.pbzy@gmail.com>
2020-04-16 19:19:49 +0900

    Add files via upload

---
+++ VarEx1.java
@@ -0,0 +1,30 @@
+
+public class VarEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(6+3); //9
+		System.out.println(6-3); //3
+		System.out.println(6*3); //18
+		System.out.println(6/3); //2
+
+		 int x = 4, y=2;
+			System.out.println(x+y); 
+			System.out.println(x-y); 
+			System.out.println(x*y); 
+			System.out.println(x/y);
+			
+		int a = 10, b = 20;
+		int tmp;
+		
+		tmp = a;
+		a = b;
+		b = tmp;
+		
+		System.out.println(a);
+		System.out.println(b);
+
+
+	}
+
+}
