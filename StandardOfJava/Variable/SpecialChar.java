9a60bf4426d73cc20604c160b9aefb79ff234529
Hannah <jeong.pbzy@gmail.com>
2020-05-12 11:47:08 +0900

    Add files via upload

---
+++ StandardOfJava/Variable/SpecialChar.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class SpecialChar {
+
+	public static void main(String[] args) {
+		char single = '\''; //single = ''';
+		String dblQuote = "\"hello\""; //큰 따옴표 출력
+		String root = "c:\\";
+		
+		System.out.println(single);
+		System.out.println(dblQuote);
+		System.out.println(root);
+
+	}
+
+}
