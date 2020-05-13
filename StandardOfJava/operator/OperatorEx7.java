8d0eed7d980b669ca9bdb4533741f253f0af9de4
Hannah <jeong.pbzy@gmail.com>
2020-05-13 20:10:15 +0900

    Add files via upload

---
+++ StandardOfJava/operator/OperatorEx7.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class OperatorEx7 {
+
+	public static void main(String[] args) {
+		boolean power = false;
+		System.out.println(power);
+		power=!power;
+		System.out.println(power); //false->true
+		power=!power;
+		System.out.println(power); //true->false
+
+	}
+
+}
