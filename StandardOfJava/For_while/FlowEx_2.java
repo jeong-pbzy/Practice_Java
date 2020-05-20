6a0a3efa0ede34032a561ab220358b60dd27b228
Hannah <jeong.pbzy@gmail.com>
2020-05-21 07:25:22 +0900

    Add files via upload

---
+++ StandardOfJava/For_while/FlowEx_2.java
@@ -0,0 +1,16 @@
+package for_while;
+
+public class FlowEx_2 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int i ;
+		
+		for (i = 1; i<=10; i++) {
+			sum += i ;
+			
+		}
+		System.out.println(i-1 + "까지의 합:"+sum);
+	}
+
+}
---
+++ StandardOfJava/For_while/FlowEx_4.java
@@ -0,0 +1,16 @@
+package for_while;
+
+public class FlowEx_4 {
+	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		for (int i =0; i<1000000000; i++) {
+			;
+		}
+		long endTime = System.currentTimeMillis();
+		
+		System.out.println("시작시간 : "+ startTime);
+		System.out.println("종료시간 : "+ endTime);
+		System.out.println("소요시간 : "+ (endTime - startTime));
+	}
+
+}
