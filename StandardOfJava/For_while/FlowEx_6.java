c8614486b4799c20faaccfbf55b6e55ee37eff78
Hannah <jeong.pbzy@gmail.com>
2020-05-22 16:00:58 +0900

    Add files via upload

---
+++ StandardOfJava/For_while/FlowEx_6.java
@@ -0,0 +1,13 @@
+package for_while;
+
+public class FlowEx_6 {
+
+	public static void main(String[] args) {
+		int i = 10;
+		while(i>=0) {
+			System.out.println(i--);
+		}
+
+	}
+
+}
---
+++ StandardOfJava/For_while/FlowEx_8.java
@@ -0,0 +1,16 @@
+package for_while;
+
+public class FlowEx_8 {
+
+	public static void main(String[] args) {
+		int sum=0;
+		int i =0;
+		
+		while (sum + i <= 100) {
+			sum +=++i; //sum = sum + ++il
+			System.out.println(i + "-"+sum);
+		}
+
+	}
+
+}
